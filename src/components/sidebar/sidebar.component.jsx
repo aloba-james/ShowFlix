5e51137e91e75bfc71714a281ece547615d9f95d
Aloba James <39866255+aloba-james@users.noreply.github.com>
2023-07-21 14:51:15 +0100

    Update sidebar.component.jsx

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -4,23 +4,26 @@ import { Link } from "react-router-dom";
 
 const SideBar = () => {
     return (
-         <div className="logo-container">
+        <>
+            <div className="logo-container">
                 <Link to='/'>
                     Logo
                 </Link>
             </div> 
-        <div className="sidebar-container">  
-            <div className="search">
-                <Link to='/'>
-                    Search
-                </Link>
+            <div className="sidebar-container">  
+                <div className="search">
+                    <Link to='/'>
+                        Search
+                    </Link>
+                </div>
+                <div className="watchlist">
+                    <Link to='/watchlist'>
+                        Watchlist
+                    </Link>
+                </div>
             </div>
-            <div className="watchlist">
-                <Link to='/watchlist'>
-                    Watchlist
-                </Link>
-            </div>
-        </div>
+        </>
+         
     );
 };
 
