1496f820680e3b3c676b4e0efa2dac6d96c5fb99
Aloba James <39866255+aloba-james@users.noreply.github.com>
2023-07-21 15:24:13 +0100

    Update moviepage.component.jsx

--- src/routes/moviepage/moviepage.component.jsx
+++ src/routes/moviepage/moviepage.component.jsx
@@ -9,29 +9,29 @@ const MoviePage = () => {
     const dataSelect = useSelector(state => state.query.data);
     return (
         <div className="moviepage-container">
-            <div>
+            <div className='title'>
                 <Link to={`/${dataSelect.Title}`}>{dataSelect.title}</Link>
             </div>
             <img src={dataSelect.Poster} alt="" />
             <div className="description">{dataSelect.Plot}</div>
-<div className='release-date'>
- {dataSelect.Released}</div>
-<div className='rating'>
- {dataSelect.imdbRating}
-</div>
-
-<div className='duration'>
- {dataSelect.Runtime}
-</div>     
+            <div className='more-info'>
+                 <div className='release-date'>
+                    {dataSelect.Released}
+                </div>
+                <div className='rating'>
+                     {dataSelect.imdbRating}
+                </div>
+                <div className='duration'>
+                     {dataSelect.Runtime}
+                </div>    
+            </div> 
             <button className='watch'>Watch Now</button>
             <div className='favorite'>Love</div>
-
-
             <div className='similar-movies'>
                 <h2>Similar Movies</h2>
                 <div></div>
             </div>
-   </div>     
+        </div>     
                
     );
 };
